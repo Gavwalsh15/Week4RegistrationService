a33015aa1ff5940c56a569e544024b5ed5d87343
Gavin <G00402296@atu.ie>
2023-10-17 16:33:12 +0100

    All Working Code

--- src/main/java/ie/atu/week4registrationservice/RegistrationController.java
+++ src/main/java/ie/atu/week4registrationservice/RegistrationController.java
@@ -1,5 +1,15 @@
 package ie.atu.week4registrationservice;
 
-public class RegistrationController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class RegistrationController {
+    @PostMapping("/confirm")
+    public String confirmDetails(@RequestBody UserDetails userDetails){
+        String confirmationMessage = String.format("Recieved details for %s with email %s",
+                userDetails.getName(),userDetails.getEmail());
+        return  confirmationMessage;
+    }
 }
